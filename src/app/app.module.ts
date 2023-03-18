2604f05995eb8c2575a431248971cd6c109c1933
Late515 <s2201201@edu.bc.fi>
2023-03-18 09:46:43 +0200

    "lisätty githubiin harjoitus tehtävä"

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule } from '@angular/forms';
+import { KahvilaComponent } from './kahvila/kahvila.component';
+import { MyyntiComponent } from './myynti/myynti.component';
+
+@NgModule({
+  declarations: [AppComponent, KahvilaComponent, MyyntiComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    FontAwesomeModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
